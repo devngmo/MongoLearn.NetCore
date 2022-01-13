4f45f0ff3e07f19ae3fdd1b772a661a531c2bdfb
TMLShared <97077711+TMLShared@users.noreply.github.com>
2022-01-13 13:49:30 +0700

    add Order

---
+++ PetShop/Domain/Entities/OrderEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop.Domain.Entities
+{
+    public class OrderEntity
+    {
+        public class Record
+        {
+            public string pet_id { get; set; }
+            public int quantity { get; set; }
+        }
+        public string _id { get; set; }
+        public string client_id { get; set; }
+
+        public List<Record> records { get; set; }
+    }
+}
---
+++ PetShop/Repositories/OrderRepository.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Driver;
+using PetShop.Domain.Entities;
+
+namespace PetShop
+{
+    public class OrderRepository
+    {
+        IMongoDatabase db;
+        IMongoCollection<OrderEntity> OrderCollection => 
+            db.GetCollection<OrderEntity>("ORDERS");
+        public OrderRepository(IMongoDatabase db)
+        {
+            this.db = db;
+        }
+        public List<OrderEntity> getOrderHistoryForClient(string clientID)
+        {
+            var result = OrderCollection
+                .Find(Builders<OrderEntity>.Filter.Eq("client_id", clientID));
+            return result.ToList();
+        }
+        public List<OrderEntity> getAllOrders()
+        {
+            var result = OrderCollection
+                .Find(Builders<OrderEntity>.Filter.Empty);
+            return result.ToList();
+        }
+        public void Clear()
+        {
+            OrderCollection.DeleteMany(Builders<OrderEntity>.Filter.Empty);
+        }
+
+        public void add(OrderEntity order)
+        {
+            OrderCollection.InsertOne(order);
+        }
+    }
+}
