ad9dd166beed89c0ce7d5689eecfdef032b16cbd
TMLShared <97077711+TMLShared@users.noreply.github.com>
2022-01-13 13:22:47 +0700

    add Utils library

---
+++ Utils/TestConfig.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System.Reflection;
+
+namespace Utils
+{
+    public class TestConfig
+    {
+        public string DBConnection { get; set; }
+        public string DBName { get; set; }
+        public int InitSamplePetCount { get; set; }
+        public static TestConfig Load(string name)
+        {
+            string workingFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string configFile = Path.Join(workingFolder, $"{name}.json");
+            if (File.Exists(configFile))
+                return JsonConvert.DeserializeObject<TestConfig>(File.ReadAllText(configFile));
+            return null;
+        }
+    }
+}
