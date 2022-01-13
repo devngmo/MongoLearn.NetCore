c0efed0d905d84b758e717fc17871da91a83bc78
TMLShared <97077711+TMLShared@users.noreply.github.com>
2022-01-13 13:49:21 +0700

    add Client

---
+++ PetShop/Domain/Entities/ClientEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop.Domain.Entities
+{
+    public class ClientEntity
+    {
+        public string _id { get; set; }
+        public string name { get; set; }
+    }
+}
---
+++ PetShop/Repositories/ClientRepository.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Driver;
+using PetShop.Domain.Entities;
+
+namespace PetShop
+{
+    public class ClientRepository
+    {
+        IMongoDatabase db;
+        IMongoCollection<ClientEntity> ClientCollection => 
+            db.GetCollection<ClientEntity>("CLIENTS");
+        public ClientRepository(IMongoDatabase db)
+        {
+            this.db = db;
+        }
+        public List<ClientEntity> getAllClients(bool nameAscending = true)
+        {
+            var result = ClientCollection
+                .Find(Builders<ClientEntity>.Filter.Empty);
+            if (nameAscending)
+                result = result.Sort(Builders<ClientEntity>.Sort.Ascending("name"));
+            else
+                result = result.Sort(Builders<ClientEntity>.Sort.Descending("name"));
+            return result.ToList();
+        }
+
+        public void Clear()
+        {
+            ClientCollection.DeleteMany(Builders<ClientEntity>.Filter.Empty);
+        }
+
+        public void add(ClientEntity client)
+        {
+            ClientCollection.InsertOne(client);
+        }
+    }
+}
